d80524b8f630d551a08aca9ade6f372f8e7d7690
hufsahaq <hufsahaq@gmail.com>
2024-10-03 14:29:44 +0100

    Initialise file

---
+++ snake_game.c
@@ -0,0 +1,23 @@
+#include <ncurses.h>
+
+int main() {
+    int screen_width = 40;
+    int screen_height = 20;
+
+    // initialise screen
+    WINDOW *win = initscr();
+
+    //take player input
+    keypad(win,true)
+    nodelay(win,true) // non blocking , program doesnt wait for input just keeps running
+
+    curs_set(0) // hides the curser
+
+    //snake
+    vec2 head = {0,0}
+    vec2 dir = {1,0}
+    
+
+    return 0;
+
+}
