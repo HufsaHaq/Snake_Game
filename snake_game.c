08329cc1b286c13ae69b0f6eaa22ffe97b0cb42a
hufsahaq <hufsahaq@gmail.com>
2024-10-06 15:28:55 +0100

    Set up

--- snake_game.c
+++ snake_game.c
@@ -1,4 +1,9 @@
-#include <ncurses.h>
+#include <curses.h>
+
+typedef struct {
+    int x;
+    int y;
+} vec2;
 
 int main() {
     int screen_width = 40;
@@ -8,14 +13,14 @@ int main() {
     WINDOW *win = initscr();
 
     //take player input
-    keypad(win,true)
-    nodelay(win,true) // non blocking , program doesnt wait for input just keeps running
+    keypad(win,true);
+    nodelay(win,true); // non blocking , program doesnt wait for input just keeps running
 
-    curs_set(0) // hides the curser
+    curs_set(0); // hides the curser
 
     //snake
-    vec2 head = {0,0}
-    vec2 dir = {1,0}
+    vec2 head = {0,0};
+    vec2 dir = {1,0};
     
 
     return 0;
