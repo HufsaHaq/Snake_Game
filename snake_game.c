0754f98177749274847cdf47a5b6a894d16d91fd
hufsahaq <hufsahaq@gmail.com>
2024-10-07 13:48:31 +0100

    Initialise game set up

--- snake_game.c
+++ snake_game.c
@@ -1,4 +1,5 @@
 #include <curses.h>
+#include <unistd.h>
 
 typedef struct {
     int x;
@@ -19,9 +20,42 @@ int main() {
     curs_set(0); // hides the curser
 
     //snake
-    vec2 head = {0,0};
+    vec2 head = {0,0}; // {x,y}
     vec2 dir = {1,0};
     
+    while(true) {
+        int pressed = wgetch(win);
+        //react to players input
+        if (pressed == KEY_LEFT){
+            dir.x = -1;
+            dir.y = 0;            
+        }
+        if (pressed == KEY_RIGHT){
+            dir.x = 1;
+            dir.y = 0;            
+        }
+        if (pressed == KEY_UP){
+            dir.x = 0;
+            dir.y = 1;            
+        }
+        if (pressed == KEY_DOWN){
+            dir.x = 0;
+            dir.y = -1;            
+        }
+        if (pressed == '\e'){ //escape key
+            break;            
+        }
+
+        head.x += dir.x;
+        head.y += dir.y;
+
+        // draw to the screen
+        erase();
+        mvaddch(head.y , head.x , '0');
+        usleep(150000);
+
+    }
+    endwin();
 
     return 0;
 
